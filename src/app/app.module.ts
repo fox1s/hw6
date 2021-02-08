1c23cbb0abb8f971ea09c1ca6b38e3cf3e9d9dcd
fox1s <ostinsmikh@gmail.com>
2021-02-08 15:22:45 +0200

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PostsComponent } from './components/posts/posts.component';
+
 import {HttpClientModule} from '@angular/common/http';
-import { PostComponent } from './components/post/post.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostsComponent,
-    PostComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
